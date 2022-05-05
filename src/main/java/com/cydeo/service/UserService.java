eba6910ab53791b5dd9c8c9e633d835ce38defaf
ozzy-cydeo <ozzy@cybertekschool.com>
2022-05-05 19:22:18 -0400

    Part-1

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -1,13 +1,8 @@
 package com.cydeo.service;
 
-import com.cydeo.dto.UserDTO;
 
-import java.util.List;
+public interface UserService{
 
-public interface UserService extends CrudService<UserDTO, String> {
 
-    List<UserDTO> findManagers();
-
-    List<UserDTO> findEmployees();
 
 }
