060c1d40f22acb420be11618651c199b37fd0861
ozzy-cydeo <ozzy@cybertekschool.com>
2022-05-07 14:30:05 -0400

    Part-2

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -77,7 +77,7 @@ public class UserController {
         return "redirect:/user/create";
 
     }
-//
+
 //    @GetMapping("/delete/{username}")
 //    public String deleteUser(@PathVariable("username") String username) {
 //        userService.deleteById(username);
