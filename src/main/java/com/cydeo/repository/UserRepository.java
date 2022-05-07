a781b55b50a1de54d6e81fd2a775f0d4010149ad
ozzy-cydeo <ozzy@cybertekschool.com>
2022-05-07 15:02:25 -0400

    Part-2

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -8,6 +8,6 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByUserName(String username);
 
-
+    @Transactional
     void deleteByUserName(String username);
 }
--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
@@ -63,5 +62,12 @@ public class UserServiceImpl implements UserService {
     public void deleteByUserName(String username) {
         userRepository.deleteByUserName(username);
 
+    }
+
+    @Override
+    public void delete(String username) {
+        User user = userRepository.findByUserName(username);
+        user.setIsDeleted(true);
+        userRepository.save(user);
     }
 }
